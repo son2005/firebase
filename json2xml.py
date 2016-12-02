389b8ca700c786513647f16bcd5bc8ea553cb340
Jianren Yin <jimmy.yin5@gmail.com>
2016-12-02 23:09:23 +0800

    update json2xml.py

--- json2xml.py
+++ json2xml.py
@@ -20,14 +20,14 @@ def google_services_json_to_xml(json_file, xml_file='google-services.xml'):
     d = json.loads(content)
 
     template = '''<?xml version="1.0" encoding="utf-8"?>
-    <resources>
-        <string name="default_web_client_id" translatable="false">{default_web_client_id}</string>
-        <string name="gcm_defaultSenderId"   translatable="false">{gcm_defaultSenderId}</string>
-        <string name="firebase_database_url" translatable="false">{firebase_database_url}</string>
-        <string name="google_app_id"         translatable="false">{google_app_id}</string>
-        <string name="google_api_key"        translatable="false">{google_api_key}</string>
-        <string name="google_storage_bucket" translatable="false">{google_storage_bucket}</string>
-    </resources>
+<resources>
+    <string name="default_web_client_id" translatable="false">{default_web_client_id}</string>
+    <string name="gcm_defaultSenderId"   translatable="false">{gcm_defaultSenderId}</string>
+    <string name="firebase_database_url" translatable="false">{firebase_database_url}</string>
+    <string name="google_app_id"         translatable="false">{google_app_id}</string>
+    <string name="google_api_key"        translatable="false">{google_api_key}</string>
+    <string name="google_storage_bucket" translatable="false">{google_storage_bucket}</string>
+</resources>
     '''
 
     ret = template.format(default_web_client_id=d['client'][0]['oauth_client'][0]['client_id']
